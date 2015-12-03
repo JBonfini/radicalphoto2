557682d74e7b9b51a1d3e981a22cd1d669ad5902
Jbonfini <justinbonfini@gmail.com>
2015-12-03 15:27:45 -0500

    Home Page
    Added the jQuery slider on the Home page. Meet all the requirements on
    "home" section on the assignment.

---
+++ js/plugins.js
@@ -0,0 +1,50 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+// Lightbox Options
+
+$(document).ready(function() {
+    // Lightbox options
+    lightbox.option({
+        'positionFromTop': 150
+    });
+    
+    //Scroll It
+    $(function(){
+        $.scrollIt();
+    });
+    
+    //Initialize Masonry
+    $(".grid").masonry({
+        "itemSelector":".grid-item",
+        "columnWidth": 0
+    });
+    
+});
+
+
+
+
+
